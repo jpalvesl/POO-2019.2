6a0a2e78e7106aadc819c959e932fdc3325ae9b4
João Lima <jpalvesl101@gmail.com>
2019-10-16 00:31:40 -0300

    Atualizando a classe de testes, faltando apenas a funcao de busca no HashSet

--- Banco_set/src/main/java/com/gmail/jpalvesl/Banco.java
+++ Banco_set/src/main/java/com/gmail/jpalvesl/Banco.java
@@ -3,7 +3,21 @@ package com.gmail.jpalvesl;
 import java.util.*;
 
 public class Banco {
-    Set<Conta> lista = Set<>;
+    Set<Conta> lista = new HashSet<>();
 
-    public adi
+
+    public boolean adicionarConta(Conta conta) {
+        return lista.add( conta );
+    }
+
+    public boolean removerConta(Conta conta) {
+        return lista.remove( conta );
+    }
+
+
+    public Conta buscarConta(int numero) {
+        lista.stream();
+        return null;
+
+    }
 }
--- Banco_set/src/test/java/com/gmail/jpalvesl/TesteBanco.java
+++ Banco_set/src/test/java/com/gmail/jpalvesl/TesteBanco.java
@@ -6,6 +6,7 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 public class TesteBanco {
+    // contas que serão usadas para insercao e remocao no banco
     static final Conta c1 = new Conta(700, 26, "João");
 
     static final Conta c2 = new Conta(650, 10, "Amjah");
@@ -14,8 +15,83 @@ public class TesteBanco {
 
     static final Conta clone_c1 = new Conta(700, 26, "João");
 
+    // banco que sera usado ao longo do teste
+    private Banco banco = new Banco();
 
     @Test
+    public void adicionarContaDeveFuncionar(){
+        // adicionando as contas no HashSet
+        banco.adicionarConta( c1 );
+        banco.adicionarConta( c2 );
+        banco.adicionarConta( c3 );
+
+        assertThat( banco.lista, containsInAnyOrder( c1, c2 , c3 ) );
+    }
+
+    @Test
+    public void adicionarContaNaoDeveFuncionar(){
+        // adicionando as contas no HashSet
+        banco.adicionarConta( c1 );
+        banco.adicionarConta( c2 );
+        banco.adicionarConta( null ); //nao deve adicionar
+        banco.adicionarConta( clone_c1 ); //nao deve adicionar
+
+        //assegurando que o clone e null nao estao no HashSet
+        assertThat( banco.lista, not( containsInAnyOrder( null, clone_c1 ) ) );
+    }
+
+    @Test
+    public void removerContaDeveFuncionar(){
+        // adicionando contas
+        banco.adicionarConta( c1 );
+        banco.adicionarConta( c2 );
+        banco.adicionarConta( c3 );
+
+        // removendo contas
+        banco.removerConta( c1 );
+        banco.removerConta( c2 );
+        banco.removerConta( c3 );
+
+        // verificando se os itens nao estao no HashSet
+        assertThat( banco.lista, not( contains( c1, c2, c3 ) ) );
+    }
+
+    @Test
+    public void removerContaNaoDeveFuncionar(){
+        // adicionando contas
+        banco.adicionarConta( c1 );
+        banco.adicionarConta( c2 );
+
+        // contas que nao devem ser removidas
+        banco.removerConta( null );
+        banco.removerConta( c3 );
+
+        assertThat( banco.lista, containsInAnyOrder( c1, c2 ) );
+    }
+
+    @Test
+    public void buscarCamisaDeveFuncionar(){
+        // adicionando contas no banco
+        banco.adicionarConta( c1 );
+        banco.adicionarConta( c2 );
+        banco.adicionarConta( c3 );
+
+        // auxiliares para guardar o retorno da funcao
+        Conta aux1, aux2, aux3;
+        aux1 = banco.buscarConta( 26 );
+        aux2 = banco.buscarConta( 10 );
+        aux3 = banco.buscarConta( 99 );
+
+        // verificando se o retorno foi de acordo com o esperado
+        assertThat( aux1, is( c1 ) );
+        assertThat( aux2, is( c2 ) );
+        assertThat( aux3, is( c3 ) );
+    }
+
+    @Test
+    public void buscarCamisaNaoDeveFuncionar(){
+
+    }
 
 
 }
