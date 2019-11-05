027135e50255fd82f6fcda41336589b0da4516aa
João Lima <jpalvesl101@gmail.com>
2019-11-05 12:43:04 -0300

    multi-modulos com o package funcionando

---
+++ multi-modulos/modulo-gui/src/main/java/myrllalp/Main.java
@@ -0,0 +1,9 @@
+package myrllalp;
+
+public class Main {
+    public static void main(String[] args) {
+        Teste t = new Teste();
+
+        System.out.println(t.nome1());
+    }
+}
---
+++ multi-modulos/modulo-persistencia/src/main/java/myrllalp/Teste.java
@@ -0,0 +1,9 @@
+package myrllalp;
+
+public class Teste {
+    private String nome="ana";
+
+    public String nome1(){
+        return this.nome;
+    }
+}
